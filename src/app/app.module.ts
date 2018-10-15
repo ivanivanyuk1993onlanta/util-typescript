c0359017d602b3c74036eeb3fcc6726b8cfcd7a8
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2018-10-16 02:10:29 +0300

    Removed bicycle components/styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {BlockComponent} from '../util/block/block.component';
-import {MenuComponent} from '../util/menu/menu.component';
-import {RouterModule} from '@angular/router';
-import {HomeComponent} from './pages/home/home.component';
-import {RouteService} from '../util/route/route.service';
-import { NotHomeComponent } from './pages/not-home/not-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlockComponent,
-    MenuComponent,
-    HomeComponent,
-    NotHomeComponent,
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(
-      RouteService.getRouteMap()
-    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/util/menu/menuData.ts
+++
@@ -1,7 +0,0 @@
-export class MenuData {
-  iClass?: string;
-  imgUrl?: string;
-  items?: MenuData[];
-  text?: string;
-  url?: string;
-}
