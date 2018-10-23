833c311ed13c70266a50a66b5ff52960da3c32fd
Ivan Ivanyuk <ivanivanyuk1993@gmail.com>
2018-10-23 05:59:57 +0300

    Played with layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
+import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,11 @@ const routes: Routes = [
   {
     component: HomeComponent,
     path: 'home',
+  },
+
+  {
+    component: PageNotFoundComponent,
+    path: '**',
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {CoreModule} from './util/core/core.module';
 import {HomeComponent} from './pages/home/home.component';
 import {NgModule} from '@angular/core';
 import {SharedModule} from './util/shared/shared.module';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     AppRoutingModule,
