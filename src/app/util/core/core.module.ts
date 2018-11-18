40611e1d505880f94e041afa79df7a127a03c027
Ivan Ivanyuk <ivanivanyuk1993@gmail.com>
2018-11-18 17:04:42 +0300

    Removed auto complete component

--- src/app/util/core/core.module.ts
+++ src/app/util/core/core.module.ts
@@ -2,7 +2,6 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import {InputAutocompletedComponent} from './input-autocompleted/input-autocompleted.component';
 import {LayoutComponent} from './layout/layout.component';
 import {LayoutModule} from '@angular/cdk/layout';
 import {MaterialUsedModule} from './material-used/material-used.module';
@@ -14,13 +13,11 @@ import {ScrollDispatchModule} from '@angular/cdk/scrolling';
 @NgModule({
   exports: [
     CommonModule,
-    InputAutocompletedComponent,
     LayoutComponent,
     MaterialUsedModule,
     RouteListModule,
   ],
   declarations: [
-    InputAutocompletedComponent,
     LayoutComponent,
   ],
   imports: [
