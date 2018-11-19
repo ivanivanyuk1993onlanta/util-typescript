e6516a672b7e33c8ac88bd446abec545c538949c
Ivan Ivanyuk <ivanivanyuk1993@gmail.com>
2018-11-19 04:01:12 +0300

    Added search with auto complete

--- src/app/util/core/route-list/route-list-root/route-list-root.component.ts
+++ src/app/util/core/route-list/route-list-root/route-list-root.component.ts
@@ -1,21 +1,58 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
 import {escapeRegExp} from 'tslint/lib/utils';
 import {FormControl} from '@angular/forms';
 import {map, startWith} from 'rxjs/operators';
 import {Observable} from 'rxjs';
 import {RouteData} from '../route-data';
+import {Router} from '@angular/router';
+import {MatOptionSelectionChange} from '@angular/material';
 
 @Component({
   selector: 'app-route-list-root',
   templateUrl: './route-list-root.component.html',
   styleUrls: ['./route-list-root.component.scss'],
 })
+
 export class RouteListRootComponent implements OnInit {
   @Input() routeDataList: RouteData[];
+  routeDataListFlat: RouteData[];
   searchRegExp$: Observable<RegExp>;
   searchString = new FormControl('');
 
-  constructor() {
+  constructor(
+    private router: Router,
+  ) {
+  }
+
+  static appendRouteDataToRouteDataListFlat(
+    routeData: RouteData,
+    routeDataListFlat: RouteData[],
+  ): void {
+    routeDataListFlat.push(routeData);
+
+    if (routeData.childRouteList) {
+      for (const routeDataChild of routeData.childRouteList) {
+        RouteListRootComponent.appendRouteDataToRouteDataListFlat(
+          routeDataChild,
+          routeDataListFlat,
+        );
+      }
+    }
+  }
+
+  assembleRouteDataListFlat(): RouteData[] {
+    const routeDataListFlat: RouteData[] = [];
+
+    for (const routeData of this.routeDataList) {
+      RouteListRootComponent.appendRouteDataToRouteDataListFlat(routeData,
+        routeDataListFlat);
+    }
+
+    return routeDataListFlat;
   }
 
   ngOnInit() {
@@ -25,6 +62,21 @@ export class RouteListRootComponent implements OnInit {
         return new RegExp(escapeRegExp(searchString), 'i');
       }),
     );
+
+    this.routeDataListFlat = this.assembleRouteDataListFlat();
+  }
+
+  selectOption(
+    event: MatOptionSelectionChange,
+    routeData: RouteData,
+  ): void {
+    if (event.source.selected) {
+      this.router.navigateByUrl(routeData.route).then(
+        () => {
+          this.searchString.setValue('');
+        },
+      );
+    }
   }
 
 }
--- src/app/util/core/route-list/route-list.module.ts
+++ src/app/util/core/route-list/route-list.module.ts
@@ -1,11 +1,11 @@
 import {CommonModule} from '@angular/common';
 import {MaterialUsedModule} from '../material-used/material-used.module';
 import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
 import {RouteListRecursiveComponent} from './route-list-recursive/route-list-recursive.component';
 import {RouteListRootComponent} from './route-list-root/route-list-root.component';
 import {RouterModule} from '@angular/router';
 import {RouteTranslationComponent} from './route-translation/route-translation.component';
-import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   exports: [
