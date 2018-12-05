42e2878f1ec8f4a7fe4722fab42e2a6bc0081c22
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2018-12-05 23:12:19 +0300

    Added service worker support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {CoreModule} from './util/core/core.module';
+import {environment} from '../environments/environment';
 import {HomeComponent} from './pages/home/home.component';
 import {NgModule} from '@angular/core';
 import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
 import {SharedModule} from './util/shared/shared.module';
 
 @NgModule({
@@ -17,6 +19,10 @@ import {SharedModule} from './util/shared/shared.module';
     AppRoutingModule,
     CoreModule,
     SharedModule,
+    ServiceWorkerModule.register(
+      'ngsw-worker.js',
+      {enabled: environment.production},
+    ),
   ],
   providers: [],
 })
