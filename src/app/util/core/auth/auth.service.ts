54bb1626bd23518f7f33b73d0528ec5dcf7d0ea1
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2018-12-09 11:56:56 +0300

    Added route data list get logic to auth service

--- src/app/util/core/auth/auth.service.ts
+++ src/app/util/core/auth/auth.service.ts
@@ -1,7 +1,9 @@
-import {Injectable} from '@angular/core';
 import {FormControl} from '@angular/forms';
-import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
 import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {RouteData} from '../route/route-data';
 import {StorageWrap} from '../storage/storage';
 
 @Injectable({
@@ -9,17 +11,15 @@ import {StorageWrap} from '../storage/storage';
 })
 export class AuthService {
   readonly isLoggedIn$: Observable<boolean>;
+  readonly routeDataList = new FormControl([]);
+  readonly routeDataListUrl = 'http://localhost:8080/route';
   readonly storage: StorageWrap;
-  readonly userName = new FormControl;
+  readonly userName = new FormControl('');
 
-  constructor() {
+  constructor(
+    private httpClient: HttpClient,
+  ) {
     this.storage = new StorageWrap('auth');
-
-    this.userName.valueChanges.subscribe(
-      (userName: string): void => {
-        this.storage.set<string>('userName', userName);
-      },
-    );
 
     this.isLoggedIn$ = this.userName.valueChanges.pipe(
       map((userName: string): boolean => {
@@ -27,12 +27,61 @@ export class AuthService {
       }),
     );
 
-    this.storage.get<string>('userName').
-      then((userName: string): void => {
-        this.userName.setValue(userName !== null ? userName : '');
+    this.registerRouteListObservers();
+    this.registerUserNameObservers();
+  }
+
+  registerRouteListObservers() {
+    const storageRecordName = 'routeDataList';
+    const defaultValue = [];
+
+    this.routeDataList.valueChanges.subscribe(
+      (routeDataList: RouteData[]): void => {
+        this.storage.set<RouteData[]>(storageRecordName, routeDataList);
+      },
+    );
+
+    this.storage.get<RouteData[]>(storageRecordName).
+      then((routeDataListStored: RouteData[]): void => {
+        if (routeDataListStored !== null) {
+          this.routeDataList.setValue(routeDataListStored);
+        } else {
+          this.httpClient.
+            get<RouteData[]>(this.routeDataListUrl).
+            toPromise<RouteData[]>().
+            then((routeDataList: RouteData[]) => {
+              this.routeDataList.setValue(routeDataList);
+            }).
+            catch(() => {
+              this.routeDataList.setValue(defaultValue);
+            });
+        }
       }).
       catch(() => {
-        this.userName.setValue('');
+        this.routeDataList.setValue(defaultValue);
+      });
+  }
+
+  registerUserNameObservers() {
+    const storageRecordName = 'userName';
+    const defaultValue = '';
+
+    this.userName.valueChanges.subscribe(
+      (userName: string): void => {
+        this.storage.set<string>(storageRecordName, userName);
+      },
+    );
+
+    this.storage.get<string>(storageRecordName).
+      then((userName: string): void => {
+        if (userName !== null) {
+          this.userName.setValue(userName);
+        } else {
+          this.userName.setValue(defaultValue);
+        }
+      }).
+      catch(() => {
+        this.userName.setValue(defaultValue);
       });
   }
 
