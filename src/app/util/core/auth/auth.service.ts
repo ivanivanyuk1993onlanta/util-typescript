002bf40877c368a8d6d25e2d4aa4b303c0e8229c
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2018-12-11 11:26:40 +0300

    Changed apiUrl environment to be consistent with Origin request header

--- src/app/util/core/auth/auth.service.ts
+++ src/app/util/core/auth/auth.service.ts
@@ -13,7 +13,7 @@ import {StorageWrap} from '../storage/storage';
 export class AuthService {
   private _isSignedInFormControl = new FormControl(false);
   private _routeDataListFormControl = new FormControl(new Array<RouteData>());
-  private _routeDataListUrl = `${environment.apiUrl}route`;
+  private _routeDataListUrl = `${environment.apiUrl}/route`;
   private _storage: StorageWrap;
   private _userNameFormControl = new FormControl('');
 
