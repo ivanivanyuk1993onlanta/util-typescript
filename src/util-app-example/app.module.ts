712b5dc7d040e049b94c20153292b656797982e6
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2019-02-27 15:13:11 +0300

    Added app example

---
+++ src/util-app-example/app.module.ts
@@ -0,0 +1,26 @@
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {CoreModule} from '../util/module/core/core.module';
+import {environment} from '../environments/environment';
+import {NgModule} from '@angular/core';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {SharedModule} from '../util/module/shared/shared.module';
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    ServiceWorkerModule.register(
+      'ngsw-worker.js',
+      {enabled: environment.production},
+    ),
+  ],
+  providers: [],
+})
+export class AppModule {
+}
