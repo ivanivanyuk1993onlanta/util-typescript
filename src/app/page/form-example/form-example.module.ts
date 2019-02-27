c98f27fd6170ecc93c55f4dc342bf532f3d0f203
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2019-02-27 13:19:41 +0300

    Added form to example page

--- src/app/page/form-example/form-example.module.ts
+++ src/app/page/form-example/form-example.module.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {FormExampleComponent} from './form-example/form-example.component';
 import {FormExampleRoutingModule} from './form-example-routing.module';
+import {SharedModule} from '../../../util/module/shared/shared.module';
 
 @NgModule({
   declarations: [FormExampleComponent],
   imports: [
     FormExampleRoutingModule,
+    SharedModule,
   ],
 })
 export class FormExampleModule {
