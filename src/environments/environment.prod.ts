2658d00f89ee8b6e9ea541ce99d4e832e015d5bd
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2019-02-28 07:35:56 +0300

    Fixed prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
+import * as logTypeWithUiPermissionDataProto from '../app/model/proto/log-type/log-type-with-ui-permission-data_pb.js';
+
 export const environment = {
   apiUrl: 'http://localhost:8080',
   production: true,
+  tableNameToTableProtoMap: {
+    'log_type': logTypeWithUiPermissionDataProto.LogTypeWithUiPermissionData,
+  },
+  tableNameToTableUrlMap: {
+    'log_type': 'log-type',
+  },
 };
