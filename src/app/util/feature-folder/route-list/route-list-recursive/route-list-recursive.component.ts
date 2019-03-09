505bc16df99766bb1e382c82b651fcf6ed71c432
ivaivanyuk1993 <ivaivanyuk1993@gmail.com>
2019-03-10 00:30:12 +0300

    Did some refactoring, added some todos

--- src/app/util/feature-folder/route-list/route-list-recursive/route-list-recursive.component.ts
+++ src/app/util/feature-folder/route-list/route-list-recursive/route-list-recursive.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectorRef, Component, Input, OnChanges, OnDestroy} from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy} from '@angular/core';
 import {distinctUntilChanged, takeUntil} from 'rxjs/operators';
 import {BehaviorSubject, Subject} from 'rxjs';
 import {RouteData} from '../../../class/route/route-data';
@@ -15,66 +15,19 @@ export class RouteListRecursiveComponent implements OnChanges, OnDestroy {
 
   private _isComponentDestroyedSubject$ = new Subject();
 
-  constructor(
-    private _changerDetectorRef: ChangeDetectorRef,
-  ) {
-  }
-
   public ngOnChanges() {
-    // todo refactor this thing!
-    console.log(`ngOnChanges: ${ (new Date()).toLocaleTimeString() }`); // todo remove debug code, make routeDataList correctly show when it is changed when search string is filled
-    for (const routeData of this.routeDataList) {
-      routeData.matchesSearchRegExpSubject$ = new BehaviorSubject<boolean>(true);
-      this.searchRegExpSubject$.pipe(
-        takeUntil(this._isComponentDestroyedSubject$),
-      ).subscribe((searchRegExp: RegExp) => {
-        routeData.matchesSearchRegExpSubject$.next(
-          searchRegExp.test(routeData.textTranslated),
-        );
-      });
-
-      if (routeData.childRouteList) {
-        routeData.countOfFilteredChildRouteListSubject$ = new BehaviorSubject<number>(0);
-        routeData.hasFilteredChildRouteListSubject$ = new BehaviorSubject<boolean>(false);
-        routeData.countOfFilteredChildRouteListSubject$.pipe(
-          takeUntil(this._isComponentDestroyedSubject$),
-        ).subscribe((countOfFilteredChildRouteList: number) => {
-          routeData.hasFilteredChildRouteListSubject$.next(countOfFilteredChildRouteList > 0);
-        });
-      }
-    }
-
     if (this.parentRouteData) {
       for (const routeData of this.routeDataList) {
-        routeData.matchesSearchRegExpSubject$.pipe(
-          // distinctUntilChanged(), // todo
-          takeUntil(this._isComponentDestroyedSubject$),
-        ).subscribe((matchesSearchRegExp: boolean) => {
-          const countOfParentReference = this.parentRouteData.countOfFilteredChildRouteListSubject$;
-          if (matchesSearchRegExp) {
-            countOfParentReference.next(countOfParentReference.getValue() + 1);
-          } else {
-            countOfParentReference.next(countOfParentReference.getValue() - 1);
-          }
-        });
-
-        if (routeData.hasFilteredChildRouteListSubject$) {
-          routeData.hasFilteredChildRouteListSubject$.pipe(
-            // distinctUntilChanged(), // todo
-            takeUntil(this._isComponentDestroyedSubject$),
-          ).subscribe((hasFilteredChildRouteList: boolean) => {
-            const countOfParentReference = this.parentRouteData.countOfFilteredChildRouteListSubject$;
-            if (hasFilteredChildRouteList) {
-              countOfParentReference.next(countOfParentReference.getValue() + 1);
-            } else {
-              countOfParentReference.next(countOfParentReference.getValue() - 1);
-            }
-          });
-        }
+        this._addMatchesSearchRegExpSubjectToRouteData(routeData);
+        this._addHasFilteredChildRouteListSubjectToRouteData(routeData);
+        this._addHasFilteredChildRouteListSubjectToParentRouteData(routeData);
+      }
+    } else {
+      for (const routeData of this.routeDataList) {
+        this._addMatchesSearchRegExpSubjectToRouteData(routeData);
+        this._addHasFilteredChildRouteListSubjectToRouteData(routeData);
       }
     }
-
-    this._changerDetectorRef.detectChanges();
   }
 
   public ngOnDestroy(): void {
@@ -82,4 +35,62 @@ export class RouteListRecursiveComponent implements OnChanges, OnDestroy {
     this._isComponentDestroyedSubject$.complete();
   }
 
+  private _addHasFilteredChildRouteListSubjectToParentRouteData(
+    routeData: RouteData,
+  ) {
+    routeData.matchesSearchRegExpSubject$.pipe(
+      distinctUntilChanged(),
+      takeUntil(this._isComponentDestroyedSubject$),
+    ).subscribe((matchesSearchRegExp: boolean) => {
+      const countOfParentReference = this.parentRouteData.countOfFilteredChildRouteListSubject$;
+      if (matchesSearchRegExp) {
+        countOfParentReference.next(countOfParentReference.getValue() + 1);
+      } else {
+        countOfParentReference.next(countOfParentReference.getValue() - 1);
+      }
+    });
+
+    if (routeData.hasFilteredChildRouteListSubject$) {
+      routeData.hasFilteredChildRouteListSubject$.pipe(
+        distinctUntilChanged(),
+        takeUntil(this._isComponentDestroyedSubject$),
+      ).subscribe((hasFilteredChildRouteList: boolean) => {
+        const countOfParentReference = this.parentRouteData.countOfFilteredChildRouteListSubject$;
+        if (hasFilteredChildRouteList) {
+          countOfParentReference.next(countOfParentReference.getValue() + 1);
+        } else {
+          countOfParentReference.next(countOfParentReference.getValue() - 1);
+        }
+      });
+    }
+  }
+
+  private _addHasFilteredChildRouteListSubjectToRouteData(
+    routeData: RouteData,
+  ) {
+    if (routeData.childRouteList) {
+      routeData.countOfFilteredChildRouteListSubject$ = new BehaviorSubject<number>(0);
+      routeData.hasFilteredChildRouteListSubject$ = new BehaviorSubject<boolean>(false);
+      routeData.countOfFilteredChildRouteListSubject$.pipe(
+        takeUntil(this._isComponentDestroyedSubject$),
+      ).subscribe((countOfFilteredChildRouteList: number) => {
+        routeData.hasFilteredChildRouteListSubject$.next(countOfFilteredChildRouteList > 0);
+      });
+    }
+  }
+
+  private _addMatchesSearchRegExpSubjectToRouteData(
+    routeData: RouteData,
+  ) {
+    // todo make routeData.textTranslated some observable/subject (it is undefined on component creation, hence when searchRegExp is not empty, it does not match it)
+    // todo check if the problem of empty filtered list when route data is changed when search string is not empty is solved
+    routeData.matchesSearchRegExpSubject$ = new BehaviorSubject<boolean>(true);
+    this.searchRegExpSubject$.pipe(
+      takeUntil(this._isComponentDestroyedSubject$),
+    ).subscribe((searchRegExp: RegExp) => {
+      routeData.matchesSearchRegExpSubject$.next(
+        searchRegExp.test(routeData.textTranslated),
+      );
+    });
+  }
 }
