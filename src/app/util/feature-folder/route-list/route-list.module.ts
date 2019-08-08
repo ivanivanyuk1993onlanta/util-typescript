f7d6641d6690475b8a6838680314707a05c9a7d1
i.ivanyuk <i.ivanyuk@axelot.ru>
2019-08-08 22:25:26 +0300

    Added empty mat-autocomplete placeholder

--- src/app/util/feature-folder/route-list/route-list.module.ts
+++ src/app/util/feature-folder/route-list/route-list.module.ts
@@ -3,12 +3,12 @@ import {CommonModule} from '@angular/common';
 import {RouteListComponent} from './route-list/route-list.component';
 import {RouteListItemComponent} from './route-list-item/route-list-item.component';
 import {RouterModule} from '@angular/router';
-import {MatExpansionModule, MatListModule} from '@angular/material';
+import {MatAutocompleteModule, MatExpansionModule, MatFormFieldModule, MatInputModule, MatListModule} from '@angular/material';
 
 @NgModule({
   declarations: [RouteListComponent, RouteListItemComponent],
   exports: [RouteListComponent],
-  imports: [CommonModule, RouterModule, MatExpansionModule, MatListModule],
+  imports: [CommonModule, RouterModule, MatExpansionModule, MatListModule, MatAutocompleteModule, MatFormFieldModule, MatInputModule],
 })
 export class RouteListModule {
 }
