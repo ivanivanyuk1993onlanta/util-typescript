89b6ce88788e16701b9268dc12f9031a69c49dbf
i.ivanyuk <i.ivanyuk@axelot.ru>
2019-08-17 00:33:34 +0300

    Turned off ChangeDetectionStrategy.OnPush to correct transclusion

--- src/app/util/feature-folder/layout/layout/layout.component.ts
+++ src/app/util/feature-folder/layout/layout/layout.component.ts
@@ -1,8 +1,7 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {Component} from '@angular/core';
 import {MediaQueryObserverService} from '../../media-query-observer/media-query-observer.service';
 
 @Component({
-  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-layout',
   styleUrls: ['./layout.component.scss'],
   templateUrl: './layout.component.html',
