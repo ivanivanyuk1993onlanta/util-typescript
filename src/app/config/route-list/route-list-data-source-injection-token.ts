d08ddde08fc05a3b56c7c462c73b4bfa95352cd0
i.ivanyuk <i.ivanyuk@axelot.ru>
2019-08-16 23:10:50 +0300

    Added icons to route list, added localization to some components, changed RouteListDataSource to work with existing app

--- src/app/config/route-list/route-list-data-source-injection-token.ts
+++ src/app/config/route-list/route-list-data-source-injection-token.ts
@@ -1,8 +1,12 @@
 import {inject, InjectionToken} from '@angular/core';
 import {RouteListDataSource} from './route-list-data-source';
 import {LocalizationService} from '../../util/feature-folder/localization/localization/localization.service';
+import {HttpClient} from '@angular/common/http';
 
 export const ROUTE_LIST_DATA_SOURCE = new InjectionToken<RouteListDataSource>('Manually constructed RouteListDataSource', {
   providedIn: 'root',
-  factory: () => new RouteListDataSource(inject(LocalizationService)),
+  factory: () => new RouteListDataSource(
+    inject(HttpClient),
+    inject(LocalizationService),
+  ),
 });
