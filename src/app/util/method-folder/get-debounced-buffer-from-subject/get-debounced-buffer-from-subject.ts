8a06cc76d806d3d5a9e13196f0a761b5e84b046f
Ivan Ivanyuk <39015659+ivanivanyuk1993@users.noreply.github.com>
2019-09-23 23:29:45 +0300

    Update get-debounced-buffer-from-subject.ts

--- src/app/util/method-folder/get-debounced-buffer-from-subject/get-debounced-buffer-from-subject.ts
+++ src/app/util/method-folder/get-debounced-buffer-from-subject/get-debounced-buffer-from-subject.ts
@@ -6,6 +6,12 @@ import {buffer, debounceTime} from 'rxjs/operators';
 // запросы для каждой потребности, а сформировать общий запрос для пришедших запросов. По умолчанию debounceTime2 = 0, что
 // положит событие в конец EventLoop
 
+// This method is for cases when we can get multiple sequential requests to some url
+// It waits for all values, then forms list of values that came and send request.
+// That allows to send not multiple api calls(limited by 6 sequential(23.09.2019)), but one api call
+// By default, debounceTime2 = 0, which means that timer will wait for all events in EventLoop, but wont't
+// wait longer
+
 export function getDebouncedBufferFromSubject$<T>(
   subject: Subject<T>,
   debounceTime2 = 0,
