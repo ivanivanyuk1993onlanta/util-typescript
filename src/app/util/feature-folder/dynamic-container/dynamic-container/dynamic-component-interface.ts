787e08bc5cbe45034f3808c97375585ad8157e50
Ivan Ivanyuk <39015659+ivanivanyuk1993@users.noreply.github.com>
2019-09-29 03:30:23 +0300

    Added todo

--- src/app/util/feature-folder/dynamic-container/dynamic-container/dynamic-component-interface.ts
+++ src/app/util/feature-folder/dynamic-container/dynamic-container/dynamic-component-interface.ts
@@ -1,5 +1,6 @@
 import {OnChanges} from '@angular/core';
-
+// Todo make input BehaviorSubject, do not extend OnChanges, which will help
+//  to not care about change detection/OnChanges call/unnecessary in some cases interface, only next call
 export interface DynamicComponentInterface<InputType> extends OnChanges {
   input: InputType;
 }
