ff86c7729dbdcd749d349845066bedd9c681b564
ivaivanyuk1993 <ivaivanyuk1993@gmail.com>
2019-10-02 23:34:02 +0300

    Updated scripts

--- .subtree/add.sh
+++ .subtree/add.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 source "$(realpath $(dirname "$0"))/variables.sh"
 
-git remote add ${DIRECTORY_NAME} ${REPOSITORY_URL}
 git subtree add --prefix ${DIRECTORY_NAME} ${REPOSITORY_URL} ${BRANCH_NAME} --squash
