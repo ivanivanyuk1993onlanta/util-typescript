3e8282f9b882d15a0b1ba2f8efc24bcd341b478e
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2019-10-21 22:00:19 +0300

    Played with moving .subtree to .subtree repository folder

--- .subtree/pull.sh
+++ .subtree/pull.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-echo $(realpath $(dirname "$0"))
+echo "$(realpath $(dirname "$0"))/variables.sh"
 source "$(realpath $(dirname "$0"))/variables.sh"
 git subtree pull --prefix ${DIRECTORY_NAME} ${REPOSITORY_URL} ${BRANCH_NAME} --squash
