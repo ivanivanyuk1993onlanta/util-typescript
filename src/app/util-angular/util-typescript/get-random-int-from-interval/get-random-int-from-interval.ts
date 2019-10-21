855cd301d39258364492d5480becc04f0d355ca7
ivanivanyuk1993 <ivanivanyuk1993@gmail.com>
2019-10-21 22:50:53 +0300

    Removed util-typescript

--- src/app/util-angular/util-typescript/get-random-int-from-interval/get-random-int-from-interval.ts
+++
@@ -1,9 +0,0 @@
-// Todo move to folder, add tests
-
-// min and max inclusively
-export function getRandomIntFromInterval(
-  min: number,
-  max: number,
-): number {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
