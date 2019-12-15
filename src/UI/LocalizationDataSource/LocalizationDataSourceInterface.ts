86e535be6da8a3ad388647fe57930517233876d5
ivanivanyuk <ivanivanyuk1993@gmail.com>
2019-12-15 23:08:38 +0300

    Added LocalizationDataSourceInterface

---
+++ src/UI/LocalizationDataSource/LocalizationDataSourceInterface.ts
@@ -0,0 +1,28 @@
+import { Observable } from "rxjs";
+
+export interface LocalizationDataSourceInterface {
+  /**
+   * Property implementation should contain Observable with current locale code
+   */
+  readonly currentLocaleContinuous$: Observable<string>;
+
+  /**
+   * Property implementation should contain Observable with list of available
+   * locales
+   */
+  readonly availableLocaleListContinuous$: Observable<string[]>;
+
+  /**
+   * Method implementation should return localized message for code for current
+   * locale
+   * @param messageCode
+   */
+  getLocalizedMessageContinuous$(messageCode: string): Observable<string>;
+
+  /**
+   * Method implementation should set current locale and return void on set
+   * completion
+   * @param locale
+   */
+  setLocale$(locale: string): Observable<void>;
+}
