143d11ed1a37e73a42ff2e2fbb13606f7c79d11d
Ivan Ivanyuk <39015659+ivanivanyuk1993@users.noreply.github.com>
2019-12-22 02:24:13 +0300

    Update FieldMessageDataType.ts

--- src/FieldMessageData/FieldMessageDataType.ts
+++ src/FieldMessageData/FieldMessageDataType.ts
@@ -1,5 +1,6 @@
 // todo add constants fieldIdIndex and fieldValueIndex
 // todo add readme
+// todo add generic ValueType = unknown
 
 /**
  * This type should be used to exchange data between machines through the net
