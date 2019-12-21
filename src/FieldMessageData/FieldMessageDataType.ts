6d340ff5d0325866a1aacdadb9833a3c8abe11e1
Ivan Ivanyuk <39015659+ivanivanyuk1993@users.noreply.github.com>
2019-12-22 02:21:52 +0300

    Update FieldMessageDataType.ts

--- src/FieldMessageData/FieldMessageDataType.ts
+++ src/FieldMessageData/FieldMessageDataType.ts
@@ -1,3 +1,6 @@
+// todo add constants fieldIdIndex and fieldValueIndex
+// todo add readme
+
 /**
  * This type should be used to exchange data between machines through the net
  * to save traffic when Protobuf or other binary format is not available
